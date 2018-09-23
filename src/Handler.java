62d0a55f5cedfeb794ee1313916999a7890fa1bb
Adrian <adrian.gollmann@gmail.com>
2018-09-23 10:26:54 +0200

    added main functionality

--- src/Handler.java
+++ src/Handler.java
@@ -1,20 +1,48 @@
+import java.io.*;
 import java.net.Inet4Address;
+import java.net.*;
 import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Handler
 {
-
+    private static Scanner scanner;
     public static void main(String[] args)
     {
+        int port = 8888;
 
-        JavaClient myClient = null;
-        try
-        {
-            myClient = new JavaClient((Inet4Address) Inet4Address.getByName("141.30.207.44"), 2223 );
-        } catch (UnknownHostException e)
-        {
-            e.printStackTrace();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+
+            System.out.println("Server is listening on port " + port);
+
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected");
+
+                InputStream input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                OutputStream output = socket.getOutputStream();
+                PrintWriter writer = new PrintWriter(output, true);
+
+
+                String text;
+
+                do {
+                    System.out.println("test\n");
+                    text = reader.readLine();
+                    String reverseText = new StringBuilder(text).reverse().toString();
+                    writer.println("Server: " + reverseText);
+
+                } while (!text.equals("bye"));
+
+                socket.close();
+            }
+
+        } catch (IOException ex) {
+            System.out.println("Server exception: " + ex.getMessage());
+            ex.printStackTrace();
         }
-        myClient.startClient();
     }
 }
